2c05341c27114b9f514dc3b86a37d8679ab36946
Alexandr <89970757+AladinAlex@users.noreply.github.com>
2022-07-21 00:02:10 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ubereats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
